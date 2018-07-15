b275cee23185e11742d74e98485c469e06427f44
nadcdc <nadcdc4@gmail.com>
2018-07-16 05:51:20 +0900

    과제중. 일단 dev모드 rds 연결해서 유저 생성 안되는것 확인->sqlite와 분리. eb deploy해보려고 일단 커밋함.

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,24 +1,37 @@
 from .base import *
 
+secrets = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
+
 DEBUG = True
 ALLOWED_HOSTS = []
 
+
+# django-storages
+INSTALLED_APPS += [
+    'storages',
+]
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+AWS_DEFAULT_ACL = secrets['AWS_DEFAULT_ACL']
+AWS_S3_REGION_NAME = secrets['AWS_S3_REGION_NAME']
+AWS_S3_SIGNATURE_VERSION = secrets['AWS_S3_SIGNATURE_VERSION']
+
 # Static
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
-# EC2_DEPLOY = os.path.dirname(BASE_DIR)
-# MEDIA_ROOT = os.path.join(EC2_DEPLOY,'.media')
-# MEDIA_URL ='/media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+
 
 # wsgi
 WSGI_APPLICATION = 'config.wsgi.dev.application'
 
 # DB
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = secrets['DATABASES']
 
 STATIC_URL = '/static/'
+
+print(DATABASES)
