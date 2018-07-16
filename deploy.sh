0f3490d851d6c962b3d67a80573ed7c0c6dca430
nadcdc <nadcdc4@gmail.com>
2018-07-16 14:33:38 +0900

    런 서버 애러수정

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 
 
-#pipenv shell
+# requirements만들기
 pipenv lock --requirements > requirements.txt
 
-git add requirements.txt
-git add -f .secrets
+# .secrets와 requriements를 starging area에 추가.
+git add -f .secrets/ requirements.txt
 
+# eb deploy 실행.
 eb deploy --profile fc-8th-eb --staged
 
+# .secrets와 requirements를 staging area에서 제거
 git reset HEAD requirements.txt, .secrets
 
+# requriements.txt 삭제
 rm -f requirements.txt
-
-#pipenv exit
-
-#git add -A
-#git reset HEAD requirements.txt
-#git reset HEAD .secrets
-#git commit -m 'Lets deploy'
-
